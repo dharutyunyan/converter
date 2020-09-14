2d26a889650df79aa5c909b9ac57f13d7cc03a15
david <davhar777@gmail.com>
2020-09-14 12:57:30 +0400

    fix translation strings

--- src/js/translations/en.js
+++ src/js/translations/en.js
@@ -2,4 +2,9 @@ export const ENGLISH_TRANSLATIONS = {
     'equals': "equals",
     'Euro': "Euro",
     'US Dollar': "US Dollar",
+    'Schweizer Franken': "Schweizer Franken",
+    'Britisch Pfund': "Britisch Pfund",
+    'Schweizer Franken': "Schweizer Franken",
+    'CAD': "CAD",
+    'JPY': "JPY",
 };
